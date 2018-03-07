090534083542289813d3e6ed9f6a29a73a623d21
weh <wb-weh343148@alibaba-inc.com>
2018-03-07 18:13:28 +0800

    first commit

---
+++ vue-music/src/components/singer/singer.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus" type="text/stylus">
+
+</style>
